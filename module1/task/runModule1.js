7ee447b95ebabcbb0c75052d61b12b27e4ac0eef
tokarandrii <tokar.andrii@gmail.com>
2018-07-27 12:31:32 +0300

    initial commit

---
+++ module1/task/runModule1.js
@@ -0,0 +1,5 @@
+const person = require('person'); // вставьте сюда путь
+
+person.setName('Petya');
+person.sayHello(); // здесь в консоль должно вывестись Hello, I'm Petya
+console.log(person.name); // здесь должно быть undefined
